7c8b4609b6a51d3c3368b2b73abd8f92ff42966a
Rafael Baron <baronrafael@hotmail.com>
2020-02-08 02:07:12 -0400

    feat: add and remove persons

--- src/app/agenda/dashboard/dashboard.component.ts
+++ src/app/agenda/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Person } from '../../core/models/person';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  personForm: FormGroup;
+  persons: Array<Person>;
+  bPersonSelected: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.personForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      age: ['', Validators.required],
+      email: ['', Validators.required]
+    });
+    this.persons = [];
+  }
+
+  handlePerson(){
+    this.persons.push(this.personForm.value);
+    this.personForm.reset();
+  }
+
+  cardSelected(person){
+    this.bPersonSelected = !this.bPersonSelected;
+    console.log(person);
+  }
+
+  goToDetails(person){
+    event.cancelBubble = true;
+    console.log(person);
+  }
+
+  deletePerson(i){
+    event.cancelBubble = true;
+    this.persons.splice(i, 1);
+    this.bPersonSelected = !this.bPersonSelected;
   }
 
 }
